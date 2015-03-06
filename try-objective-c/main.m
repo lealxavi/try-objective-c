30a1541282903e67b4b494576ac9d12e182b24f4
Xavi Leal <lealxavi@gmail.com>
2015-03-06 14:46:57 +0100

    Adding a random comment to my NSLog

--- try-objective-c/main.m
+++ try-objective-c/main.m
@@ -11,7 +11,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World! (Can you commit)?");
     }
     return 0;
 }
