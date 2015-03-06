f97f75e1d53fed61b519daecae188d40ea948aa3
Xavi Leal <lealxavi@gmail.com>
2015-03-06 15:26:16 +0100

    Passing parameters to blocs

--- try-objective-c/main.m
+++ try-objective-c/main.m
@@ -9,9 +9,11 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World! (Can you commit)?");
-    }
-    return 0;
+    void (^myFirstBlock)(NSString *) = ^(NSString *word){
+        NSLog(@"%@", word);
+    };
+    
+    myFirstBlock(@"Hello");
+    myFirstBlock(@"World");
+
 }
