514ea4cc7f7ed30d3d916f11bc2295fa59719421
Xavi Leal <lealxavi@gmail.com>
2015-03-06 14:44:53 +0100

    Initial Commit

---
+++ try-objective-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  try-objective-c
+//
+//  Created by Xavier Leal Meseguer on 6/3/15.
+//  Copyright (c) 2015 Xavier Leal Meseguer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
